ae90373bd20f27de5929be99a9915d90c2dd5551
Carlos Machado <carlosmachado@edu.univali.br>
2021-07-14 15:27:24 -0300

    unit test project

---
+++ BPS Project/BPS UnitTest/BPSFileTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BPS_UnitTest
+{
+	[TestClass]
+	public class BPSFileTest
+	{
+		[TestMethod]
+		public void InsertTest()
+		{
+		}
+	}
+}
---
+++ BPS Project/BPS UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BPS UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BPS UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d261680-1ea5-4abf-b458-eabfc5665e82")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
