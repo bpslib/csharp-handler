2297c5825ab907c886731a5243ba2e0668ee97e6
Carlos Machado <carlosmachado@edu.univali.br>
2021-07-14 15:45:04 -0300

    implemented bps file unit test class

--- BPS Project/BPS UnitTest/BPSFileTest.cs
+++ BPS Project/BPS UnitTest/BPSFileTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BPSLib;
 
 namespace BPS_UnitTest
 {
@@ -7,8 +8,87 @@ namespace BPS_UnitTest
 	public class BPSFileTest
 	{
 		[TestMethod]
-		public void InsertTest()
+		public void CountTest()
 		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			file.Add("key", "value");
+			file.Add("key2", "value2");
+
+			// Assert
+			Assert.AreEqual(2, file.Count());
+		}
+
+		[TestMethod]
+		public void ContainsTest()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			file.Add("key", "value");
+			file.Add("key2", "value2");
+			file.Clear();
+
+			// Assert
+			Assert.IsTrue(file.Contains("key2"));
+		}
+
+		[TestMethod]
+		public void AddTest()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			file.Add("key", "value");
+
+			// Assert
+			Assert.AreEqual(1, file.Count());
+		}
+
+		[TestMethod]
+		public void RemoveTest()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			file.Add("key", "value");
+			file.Remove("key");
+
+			// Assert
+			Assert.AreEqual(0, file.Count());
+		}
+
+		[TestMethod]
+		public void FindTest()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			file.Add("key", "value");
+
+			// Assert
+			Assert.AreEqual("value", file.Find("key"));
+		}
+
+		[TestMethod]
+		public void ClearTest()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			file.Add("key", "value");
+			file.Add("key2", "value2");
+			file.Clear();
+
+			// Assert
+			Assert.AreEqual(0, file.Count());
 		}
 	}
 }
