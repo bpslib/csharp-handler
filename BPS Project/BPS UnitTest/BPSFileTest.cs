ecfa992d328659d694fbbc24136e2d197a1adc8a
Carlos Machado <carlosmachado@edu.univali.br>
2021-07-17 17:12:38 -0300

    added more test cases to bps file

--- BPS Project/BPS UnitTest/BPSFileTest.cs
+++ BPS Project/BPS UnitTest/BPSFileTest.cs
@@ -11,13 +11,27 @@ namespace BPS_UnitTest
 		{
 			// Arrange
 			BPSFile file = new BPSFile();
-
-			// Act
 			file.Add("key", "value");
 			file.Add("key2", "value2");
 
+			// Act
+			int count = file.Count();
+
 			// Assert
-			Assert.AreEqual(2, file.Count());
+			Assert.AreEqual(2, count);
+		}
+
+		[TestMethod]
+		public void CountTest_Empty()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			int count = file.Count();
+
+			// Assert
+			Assert.AreEqual(0, count);
 		}
 
 		[TestMethod]
@@ -25,15 +39,29 @@ namespace BPS_UnitTest
 		{
 			// Arrange
 			BPSFile file = new BPSFile();
-
-			// Act
 			file.Add("key", "value");
 			file.Add("key2", "value2");
 
+			// Act
+			bool contain = file.Contains("key2");
+
 			// Assert
-			Assert.IsTrue(file.Contains("key2"));
-			file.Clear();
-			Assert.IsFalse(file.Contains("key"));
+			Assert.IsTrue(contain);
+		}
+
+		[TestMethod]
+		public void ContainsTest_NoExist()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+			file.Add("key", "value");
+			file.Add("key2", "value2");
+
+			// Act
+			bool contain = file.Contains("key3");
+
+			// Assert
+			Assert.IsFalse(contain);
 		}
 
 		[TestMethod]
@@ -43,10 +71,29 @@ namespace BPS_UnitTest
 			BPSFile file = new BPSFile();
 
 			// Act
-			file.Add("key", "value");
+			file.Add("key0", "value");
+			file.Add("key1", "value");
+			file.Add("key2", "value");
+			file.Add("key3", "value");
 
 			// Assert
-			Assert.AreEqual(1, file.Count());
+			Assert.AreEqual(4, file.Count());
+		}
+
+		[TestMethod]
+		public void AddTest_AlreadyExist()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+
+			// Act
+			file.Add("key0", "value");
+			file.Add("key0", "value");
+			file.Add("key0", "value");
+			file.Add("key1", "value");
+
+			// Assert
+			Assert.AreEqual(2, file.Count());
 		}
 
 		[TestMethod]
@@ -54,9 +101,9 @@ namespace BPS_UnitTest
 		{
 			// Arrange
 			BPSFile file = new BPSFile();
+			file.Add("key", "value");
 
 			// Act
-			file.Add("key", "value");
 			file.Remove("key");
 
 			// Assert
@@ -64,16 +111,63 @@ namespace BPS_UnitTest
 		}
 
 		[TestMethod]
-		public void FindTest()
+		public void RemoveTest_Nothing()
 		{
 			// Arrange
 			BPSFile file = new BPSFile();
 
 			// Act
-			file.Add("key", "value");
+			file.Remove("key");
 
 			// Assert
-			Assert.AreEqual("value", file.Find("key"));
+			Assert.AreEqual(0, file.Count());
+		}
+
+		[TestMethod]
+		public void RemoveTest_MoreThanOne()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+			file.Add("key", "value");
+			file.Add("key1", "value");
+			file.Add("key2", "value");
+
+			// Act
+			file.Remove("key1");
+
+			// Assert
+			Assert.AreEqual(2, file.Count());
+		}
+
+		[TestMethod]
+		public void FindTest()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+			file.Add("key", "value");
+
+			// Act
+			string value = file.Find("key").ToString();
+
+			// Assert
+			Assert.AreEqual("value", value);
+		}
+
+		[TestMethod]
+		public void FindTest_MoreThanOne()
+		{
+			// Arrange
+			BPSFile file = new BPSFile();
+			file.Add("key", "value");
+			file.Add("key1", "value");
+			file.Add("key2", "value");
+			file.Add("key3", "value");
+
+			// Act
+			string value = file.Find("key2").ToString();
+
+			// Assert
+			Assert.AreEqual("value", value);
 		}
 
 		[TestMethod]
@@ -81,10 +175,10 @@ namespace BPS_UnitTest
 		{
 			// Arrange
 			BPSFile file = new BPSFile();
-
-			// Act
 			file.Add("key", "value");
 			file.Add("key2", "value2");
+
+			// Act
 			file.Clear();
 
 			// Assert
